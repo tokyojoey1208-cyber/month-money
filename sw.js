61fbd8b2e8127b6c658ddb722e2e6c37b685eb70
tokyojoey1208-cyber <tokyojoey1208@gmail.com>
2026-09-04 02:55:30 +0900

    Cache encrypted bootstrap assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='my-money-v2-20260904-3';
-const ASSETS=['./','./index.html','./styles.css','./patch.css','./app-v3.js','./manifest.webmanifest'];
+const CACHE='my-money-v2-20260904-4';
+const ASSETS=['./','./index.html','./styles.css','./patch.css','./snapshot-loader.js','./snapshot.enc','./app-v3.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return res;}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
